6c099548f5e00a0389a51ada354eeac576608076
unknown <text_en@mail.ru>
2023-08-21 20:59:10 +0300

    feat: configure domain layer

---
+++ LibraryAPI/WebLibrary.Domain/Requests/IdentityRequest/RegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace WebLibrary.Domain.Requests.IdentityRequest;
+
+public class RegisterRequest
+{
+    public string FirstName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string MiddleName { get; set; } = null!;
+
+    public string Login { get; set; } = null!;
+
+    public string Password { get; set; } = null!;
+}
---
+++ LibraryAPI/WebLibrary.Domain/Settings/AuthorizationSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace WebLibrary.Domain.Settings
+{
+    public class AuthorizationSettings
+    {
+        public string Secret { get; set; } = null!;
+
+        public TimeSpan TokenLifetime { get; set; }
+    }
+}
