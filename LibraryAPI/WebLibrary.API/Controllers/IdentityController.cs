cff2cf5eea509680c797c03551c86ca093e3be15
Roma Jivchik <text_en@mail.ru>
2023-08-24 11:25:22 +0300

    feat: add identity controller

---
+++ LibraryAPI/WebLibrary.API/Controllers/IdentityController.cs
@@ -0,0 +1,48 @@
+﻿using WebLibrary.Domain.DTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+using WebLibrary.BLL.Services.IdentityServices;
+using WebLibrary.Domain.Requests.IdentityRequests;
+
+namespace WebLibrary.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class IdentityController : ControllerBase
+    {
+        private readonly IIdentityService _identityService;
+
+        public IdentityController(IIdentityService identityService)
+        {
+            _identityService = identityService;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("register")]
+        public async Task<ActionResult<AuthenticationResult>> RegisterAsync([FromBody] RegisterRequest request)
+        {
+            var response = await _identityService.RegisterAsync(request);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+
+        [AllowAnonymous]
+        [HttpPost("login")]
+        public async Task<ActionResult<AuthenticationResult>> LoginAsync([FromBody] LoginRequest request)
+        {
+            var response = await _identityService.LoginAsync(request);
+
+            if (!response.Success)
+            {
+                return BadRequest(response);
+            }
+
+            return Ok(response);
+        }
+    }
+}
